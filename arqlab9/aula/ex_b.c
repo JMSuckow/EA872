2326f3c785908b098735f758c36db4dc10a7d3d1
JMSuckow <jmsuckowrodrigues@gmail.com>
2016-11-16 13:14:49 -0200

    WIP: Lab 10

--- arqlab9/aula/ex_b.c
+++ arqlab9/aula/ex_b.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 
+int test_variavel;
+
 main()
 {
 int pid;
 	printf ("pid(processo)=%d, pid(processo-pai)=%d\n",getpid(),getppid());
+	test_variavel = 10;
 	pid = fork();
 	if (pid == 0)
 	{
-		printf("\nProcesso-filho:\n");
+		test_variavel++;
+		printf("\nProcesso-filho:%d\n",test_variavel);
 		printf("pid(processo-filho)=%d, pid(processo-pai)=%d\n",getpid(),getppid());
 	}
 	else
 	{
-		printf("\nProcesso-pai:\n");
+		printf("\nProcesso-pai:%d\n",test_variavel);
 		printf("pid(processo-filho)=%d\n",pid);
 		sleep(10);
 	}
-	printf("Processo %d terminou!\n",getpid());
+	printf("Processo %d terminou!%d\n",getpid(),test_variavel);
 }
